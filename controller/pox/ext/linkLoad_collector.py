b6ba8774f6f2b5c0bde2811f6e836856b84c76d8
Ricky <riccardo.zucchelli@gmail.com>
2025-04-29 11:29:15 +0200

    working on a simpler topology

--- controller/pox/ext/linkLoad_collector.py
+++ controller/pox/ext/linkLoad_collector.py
@@ -15,22 +15,24 @@ class trafficMeter():
 		core.openflow.addListeners(self)
 
 	def _handle_ConnectionUp (self, event):
-		# if the connecting switch has the DPID reported in self.dpid, then
-		# save the connection object in self.connection
-		# run the launch_component method
+		if dpidToStr(event.dpid) == self.dpid:
+			self.connection = event.connection
+			self.launch_component()
 
 	def launch_component(self):
 		Timer(self.time_period, self.send_stat_req, recurring=True)
 
 	def _handle_FlowStatsReceived(self, event):
 		rec_bytes = 0
-		# sum the traffic stats of all the flow rules related to IP traffic from self.src to self.dst
+		for f in event.stats:
+			if f.match.nw_src == self.src and f.match.nw_dst == self.dst:
+				rec_bytes += f.byte_count
 		rate = (rec_bytes - self.traffic) / self.time_period
 		self.traffic = rec_bytes
 		print("IP flow rate: " + str(rate))
 
 	def send_stat_req(self):
-		# send a stat request OFP message to the switch
+		self.connection.send(of.ofp_stats_request(body=of.ofp_flow_stats_request()))
 
 def launch(ip_src = "10.0.0.1", ip_dst = "10.0.0.2", time_period = 2, dpid = "7a-c4-ae-1c-40-46"):
 	trafficMeter(ip_src, ip_dst, time_period, dpid)
